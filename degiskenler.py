9b21c11e998be62ed32d7a910a0713064ab25796
Kaan Turhan <kaanturhan2004@gmail.com>
2025-08-05 12:17:26 +0300

    Create degiskenler.py
    degiskenler.py dosyası eklendi – Değişkenler konusu

---
+++ degiskenler.py
@@ -0,0 +1,17 @@
+# Değişkenler - BTK Python Eğitimi Notları
+
+# Sayısal değişkenler
+sayi1 = 10
+sayi2 = 20
+toplam = sayi1 + sayi2
+print("Toplam:", toplam)
+
+# Metinsel değişkenler
+ad = "Kaan"
+soyad = "Turhan"
+tam_ad = ad + " " + soyad
+print("Ad Soyad:", tam_ad)
+
+# Boolean (Mantıksal) değişken
+ogrenci_mi = True
+print("Öğrenci misin?:", ogrenci_mi)
